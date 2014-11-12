61ac670bfbbd6144b0008660e44377d050225db6
vinoth <vinoth4.manickam@gmail.com>
2014-11-12 00:09:34 -0500

    initial

---
+++ ScriptTaskVariable/src/main/java/defaultPackage/Sample_ProcessJUnitTest.java
@@ -0,0 +1,32 @@
+package defaultPackage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jbpm.process.instance.impl.demo.SystemOutWorkItemHandler;
+import org.jbpm.test.JbpmJUnitTestCase;
+
+import org.junit.Test;
+
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.process.ProcessInstance;
+import org.kie.api.task.TaskService;
+import org.kie.api.task.model.TaskSummary;
+
+public class Sample_ProcessJUnitTest extends JbpmJUnitTestCase {
+
+	@Test
+    public void testImplicit() {
+        KieSession ksession = createKnowledgeSession("name.bpmn");
+        
+        Map<String,Object> params=new HashMap<String,Object>();
+        params.put("name", "Nadim");
+        
+        ProcessInstance processInstance = ksession.startProcess("com.sample.bpmn",params);
+        // do your checks here
+        // for example, assertProcessInstanceCompleted(processInstance.getId(), ksession);
+    }
+
+}
